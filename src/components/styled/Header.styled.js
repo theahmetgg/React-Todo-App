2b218e496869b310b22bd2b252a6ce7b0a3fd011
Ahmet Gülmez <ahmet1998gulmez@gmail.com>
2022-09-06 18:27:05 +0300

    Initial commit

---
+++ src/components/styled/Header.styled.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  height: 6rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .title {
+    color: #fff;
+    padding-top: 0.5rem;
+    letter-spacing: 10px;
+    font-weight: 700;
+  }
+
+  .toggle-icon {
+    background: none;
+    border: none;
+
+    &:hover {
+      cursor: pointer;
+      opacity: 0.75;
+    }
+  }
+
+  @media (min-width: 1440px) {
+    margin-top: 1rem;
+  }
+`;
